256aa1c4bc7f3e8aa881583600373279ba221e08
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-07-05 21:51:25 +0530

    Moved random_word to a method
    conditioned it to return only if word's length is between 5 and 12

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,6 +2,12 @@
 
 puts 'Welcome to Hangman by Clumsyknight'
 
-word_list = File.read '5desk.txt'
-words_array = word_list.split(' ')
-puts words_array.sample
+def random_word
+  word_list = File.read '5desk.txt'
+  words_array = word_list.split(' ')
+  word_sample = words_array.sample
+  word_sample = words_array.sample until word_sample.length.between?(5, 12)
+  word_sample
+end
+
+puts random_word
