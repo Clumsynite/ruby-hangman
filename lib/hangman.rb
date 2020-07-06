b4be7590b581ab8fbc731719f2d40c9f8d299d6e
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-07-06 13:59:09 +0530

    Guessed letter validation added
    Need:
    Refactoring

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -22,7 +22,7 @@ class Hangman
           But, I can't make it like that.
           So, If you make a wrong guess it'll cost you nothing, woohoo. Except for the fact that you wasted a turn.
        5. There'll be a record of your correct and wrong guesses shown to you everytime you guess
-       6. You can't guess a letter which you have guessed earlier
+       6. You can't guess a letter which you have @guessed earlier
        7. Boring part ends. Now you can play the game.
     HEREDOC
   end
@@ -46,22 +46,34 @@ class Hangman
   def guess_secret_word
     puts 'You have 10 turns to guess the Secret word set by me'
     (1..10).each do |time|
-      print "Turn #{time}: "
+      print "\nTurn #{time}: "
+      puts display_secret_word_as_dash
       ask_for_letter
       check_win_state(time)
     end
   end
 
   def ask_for_letter
-    guessed = false
+    @guessed = false
     puts 'Enter a character to guess the word'
-    until guessed
+    until @guessed
       guess = gets.chomp!.downcase.strip
-      guess.match(/[a-z]/) && guess.length == 1 ? guessed = true : (puts 'try again')
+      check_guess_letter(guess)
+    end
+  end
+
+  def check_guess_letter(guess)
+    if guess.match(/[a-z]/) && guess.length == 1 && !already_guessed?(guess)
+      @guessed = true
       @guess_word_array.push(guess)
-      puts @guess_word_array.inspect
+      puts "Letters guessed: #{@guess_word_array.join(' ')}"
       validate_guess_letter(guess)
-      puts display_secret_word_as_dash
+    elsif already_guessed?(guess)
+      puts "You have already guess this character\nTry another one"
+    elsif guess.match(/[^a-z]/)
+      puts 'Please try to enter an alphabet next time'
+    else
+      puts 'Please enter AN(one) alphabet'
     end
   end
 
@@ -69,7 +81,13 @@ class Hangman
     if @secret_word.include?(letter)
       same_index = (0...(@secret_word.length)).find_all { |i| @secret_word[i, 1] == letter }.inspect
       replace_same_index_dash(same_index, letter)
+    else
+      puts "\nThis letters is not in my word\nTry another letter"
     end
+  end
+
+  def already_guessed?(letter)
+    @guess_word_array.include?(letter)
   end
 
   def replace_same_index_dash(same_index, letter)
