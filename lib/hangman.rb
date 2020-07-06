44e4341b97a139dd3c2176296fead3266ee1f48e
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-07-06 15:06:06 +0530

    Made a Game class to start the game and display heading
    Need:
    1. Serialization to save the game
    2. Save class
    3. Load save game option on game start

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Hangman class to run the game
+# Hangman class with the game logic
 class Hangman
   attr_accessor :secret_word, :guess, :guess_word
   def initialize
@@ -8,23 +8,6 @@ class Hangman
     @guess = guess
     @guess_word_array = []
     @guess_word = guess_word
-  end
-
-  def heading
-    puts "\nWelcome to Hangman by Clumsyknight"
-    puts "\nIf you know the rules skip this"
-    puts "\nSo, the rules here are as follows"
-    puts <<-HEREDOC
-       1. I have a word with me, which you'll have to guess in 10 turns
-       2. The word to guess is represented by a row of dashes, representing each letter of the word.
-       3. If you guess a letter which occurs in the word, I'll write it in all its correct positions.
-       4. Now, normally there's a stick figure which is drawn every time you make a wrong guess
-          But, I can't make it like that.
-          So, If you make a wrong guess it'll cost you nothing, woohoo. Except for the fact that you wasted a turn.
-       5. There'll be a record of your correct and wrong guesses shown to you everytime you guess
-       6. You can't guess a letter which you have @guessed earlier
-       7. Boring part ends. Now you can play the game.
-    HEREDOC
   end
 
   def create_secret_word
@@ -40,21 +23,23 @@ class Hangman
   end
 
   def display_secret_word_as_dash
-    @dash.join(' ')
+    puts @dash.join(' ')
   end
 
   def guess_secret_word
-    puts 'You have 10 turns to guess the Secret word set by me'
-    (1..10).each do |time|
-      print "\nTurn #{time}: "
-      puts display_secret_word_as_dash
+    puts "\nYou have 10 turns to guess the Secret word set by me"
+    10.times do |time|
+      print "\n#{10 - time} Guesses left -> "
+      display_secret_word_as_dash
       ask_for_letter
+      display_secret_word_as_dash
       check_win_state(time)
     end
   end
 
   def ask_for_letter
     @guessed = false
+    puts "Letters guessed: #{@guess_word_array.join(' ')}"
     puts 'Enter a character to guess the word'
     until @guessed
       guess = gets.chomp!.downcase.strip
@@ -66,14 +51,11 @@ class Hangman
     if guess.match(/[a-z]/) && guess.length == 1 && !already_guessed?(guess)
       @guessed = true
       @guess_word_array.push(guess)
-      puts "Letters guessed: #{@guess_word_array.join(' ')}"
       validate_guess_letter(guess)
     elsif already_guessed?(guess)
       puts "You have already guess this character\nTry another one"
     elsif guess.match(/[^a-z]/)
-      puts 'Please try to enter an alphabet next time'
-    else
-      puts 'Please enter AN(one) alphabet'
+      puts 'Please enter AN alphabet'
     end
   end
 
@@ -101,29 +83,48 @@ class Hangman
   end
 
   def check_win_state(time)
-    if dash_filled? && time <= 10
-      you_won
-    elsif !dash_filled? && time == 10
-      you_lose
+    if dash_filled? && time <= 9
+      abort("\nYou won\nThe word was #{@secret_word}")
+    elsif !dash_filled? && time == 9
+      abort("\nOh no! You lost\nThe word was #{@secret_word}")
     end
   end
+end
 
-  def you_won
-    abort('You won')
+# Game class to start the game
+class Game
+  def initialize
+    @hangman = Hangman.new
   end
 
-  def you_lose
-    abort("Oh no! You lost\nThe word was #{@secret_word}")
+  def heading
+    puts "\nWelcome to Hangman by Clumsyknight"
+    puts "\nIf you know the rules, skip this part"
+    rules
+  end
+
+  def rules
+    puts "\nSo, the rules here are as follows"
+    puts <<-HEREDOC
+       1. I have a word with me, which you'll have to guess in 10 turns
+       2. The word to guess is represented by a row of dashes, representing each letter of the word.
+       3. If you guess a letter which occurs in the word, I'll write it in all its correct positions.
+       4. Now, normally there's a stick figure which is drawn every time you make a wrong guess
+          But, I can't draw something like that.
+          So, If you make a wrong guess it'll cost you nothing, Woohoo!. Except for the fact that you wasted a turn.
+       5. There'll be a list of letters you guessed shown to you everytime you guess
+       6. You can't guess a letter which you have guessed earlier
+       7. Boring part ends. Now you can play the game.
+    HEREDOC
   end
 
   def start
     heading
-    create_secret_word
-    puts @secret_word
-    create_secret_word_as_dash
-    guess_secret_word
+    @hangman.create_secret_word
+    @hangman.create_secret_word_as_dash
+    @hangman.guess_secret_word
   end
 end
 
-hangman = Hangman.new
-hangman.start
+game = Game.new
+game.start
